13901a1c6a49f896ea6a2035ae1e06f95de95b50
Gunwant Jain <mail@wantguns.dev>
2022-01-03 11:44:38 +0530

    Implement traps
    Signed-off-by: Gunwant Jain <mail@wantguns.dev>

--- src/main.c
+++ src/main.c
@@ -51,12 +51,12 @@ enum {
 
 // Trap codes
 enum {
-    TRAP_GETC = 0x20,
-    TRAP_OUT = 0x21,
-    TRAP_PUTS = 0x22,
-    TRAP_IN = 0x23,
-    TRAP_PUTSP = 0x24,
-    TRAP_HALT = 0x25,
+    TRAP_GETC = 0x20,   // get char, not echoed
+    TRAP_OUT = 0x21,    // print a char 
+    TRAP_PUTS = 0x22,   // print a word string
+    TRAP_IN = 0x23,     // get char, echoed
+    TRAP_PUTSP = 0x24,  // print a byte string
+    TRAP_HALT = 0x25,   // halt the program
 };
 
 // sign_extend for certain operations when operation occurs in immediate
@@ -282,14 +282,79 @@ int main(int argc, char* argv[]) {
                 }
                 break;
             case OP_TRAP:
-                // TODO: implement
+                switch (instr & 0xFF) {
+                    case TRAP_GETC:
+                        {
+                            reg[R_R0] = (uint16_t)getchar();
+                        }
+                        break;
+                    case TRAP_OUT:
+                        {
+                            putc((char)reg[R_R0], stdout);
+                            fflush(stdout);
+                        }
+                        break;
+                    case TRAP_PUTS:
+                        {
+                            uint16_t* c = memory + reg[R_R0];
+                            while(*c) {
+                                putc((char)*c, stdout);
+                                ++c;
+                            }
+
+                            fflush(stdout);
+                        }
+                        break;
+                    case TRAP_IN:
+                        {
+                            printf("Enter a character: ");
+                            char c = getchar();
+                            putc(c, stdout);
+
+                            reg[R_R0] = (uint16_t)c;
+                        }
+                        break;
+                    case TRAP_PUTSP:
+                        {
+                            // one char per byte (two bytes per word)
+                            // here we need to swap back to the big
+                            // endian format
+
+                            // LC3 is 16 bit. the characters are 16 bit
+                            // long, instead of the normal 8 bit (1 byte).
+                            // 
+                            // Therefore we have to chop off the extra
+                            // bits
+
+                            uint16_t* c = memory + reg[R_R0];
+                            while (*c) {
+                                char char1 = (*c) & 0xFF;
+                                putc(char1, stdout);
+                                char char2 = (*c) >> 8;
+                                if (char2) 
+                                    putc(char2, stdout);
+
+                                ++c;
+                            }
+
+                            fflush(stdout);
+                        }
+                        break;
+                    case TRAP_HALT:
+                        {
+                            puts("HALT");
+                            fflush(stdout);
+
+                            running = 0;
+                        }
+                        break;
+                }
                 break;
             case OP_RES:
-                abort();
             case OP_RTI:
-                abort();
             default:
-                // TODO: implement base opcode
+                // bad opcode
+                abort();
                 break;
         }
     }
